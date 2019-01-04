e9ec4cb4e798f940b40b3c567d8f5ca9b773b21c
DoubleH <1120023921@qq.com>
2019-01-04 17:00:37 +0800

    修改轮播对接

--- src/components/HeaderImg.vue
+++ src/components/HeaderImg.vue
@@ -8,19 +8,6 @@
   import {Swiper} from 'vux'
   import API from '@/utils/api'
 
-  const swiperList = [{
-    url: 'javascript:',
-    img: 'https://pe-1252783006.cos.ap-guangzhou.myqcloud.com/carousel/4b0a6b6b-1af1-4925-9f7f-7c9ceee2f290.jpg',
-    title: '送你一朵fua',
-  }, {
-    url: 'javascript:',
-    img: 'https://pe-1252783006.cos.ap-guangzhou.myqcloud.com/carousel/5271daa0-9f1d-4874-8498-9b7670580fa2.jpg',
-    title: '送你一辆车',
-  }, {
-    url: 'javascript:',
-    img: 'https://pe-1252783006.cos.ap-guangzhou.myqcloud.com/carousel/5bfbe12a-ea91-458c-90c4-a2fd9f0adf0b.jpg',
-    title: '送你一次旅行'
-  }]
   export default {
     name: "HeaderImg",
     data() {
