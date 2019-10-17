a1cb6e27dd8878a0925cf4ec8a243554c7d5a230
胡昊 <huhao9277@gmail.com>
2019-10-17 22:32:22 +0800

    视力上报校验

--- src/components/EyeUpdate.vue
+++ src/components/EyeUpdate.vue
@@ -4,10 +4,18 @@
     <Group title="视力信息">
       <XInput title="左眼裸力视力" v-model="eye.leftNaked" :disabled="eye.status===1"></XInput>
       <XInput title="右眼裸眼视力" v-model="eye.rightNaked" :disabled="eye.status===1"></XInput>
-      <XInput title="左眼串镜" v-model="eye.leftGlass" :disabled="eye.status===1"></XInput>
-      <XInput title="右眼串镜" v-model="eye.rightGlass" :disabled="eye.status===1"></XInput>
-      <XInput title="左眼屈光不正" v-model="eye.leftAmetropia" :disabled="eye.status===1"></XInput>
-      <XInput title="右眼屈光不正" v-model="eye.rightAmetropia" :disabled="eye.status===1"></XInput>
+      <Group title="">
+        <Selector title="左眼串镜" v-model="eye.leftGlass" :options="glassList"></Selector>
+      </Group>
+      <Group title="">
+        <Selector title="右眼串镜" v-model="eye.rightGlass" :options="glassList"></Selector>
+      </Group>
+      <Group title="">
+        <Selector title="左眼屈光不正" v-model="eye.leftAmetropia" :options="ametropiaList"></Selector>
+      </Group>
+      <Group title="">
+        <Selector title="右眼屈光不正" v-model="eye.rightAmetropia" :options="ametropiaList"></Selector>
+      </Group>
       <XInput title="状态" v-model="eye.statusView" :disabled="true"></XInput>
     </Group>
     <div style="padding:15px;">
@@ -17,7 +25,7 @@
 </template>
 
 <script>
-    import {CellBox, Cell, Group, XHeader, XInput, FlexboxItem, Flexbox, XButton} from 'vux'
+    import {CellBox, Cell, Group, XHeader, XInput, FlexboxItem, Flexbox, XButton, Selector} from 'vux'
     import API from '@/utils/api'
 
     export default {
@@ -31,7 +39,19 @@
                     leftAmetropia: "",
                     rightAmetropia: "",
                     status: 0
-                }
+                },
+                glassList: [
+                    {key: '0 正常', value: '0 正常'},
+                    {key: '1 远视', value: '1 远视'},
+                    {key: '-1 近视', value: '-1 近视'},
+                    {key: '2 疾病', value: '2 疾病'}
+                ],
+                ametropiaList: [
+                    {key: '0 正常', value: '0 正常'},
+                    {key: '1 近视', value: '1 近视'},
+                    {key: '2 远视', value: '2 远视'},
+                    {key: '3 疾病', value: '3 疾病'}
+                ]
             }
         },
         methods: {
@@ -69,6 +89,38 @@
             //更新学期
             insertEye() {
                 let self = this;
+                if (self.eye.leftNaked != 0 && !(self.eye.leftNaked >= 3.0 && self.eye.leftNaked <= 5.3)) {
+                    alert('左眼视力录入范围3.0-5.3之间，若低于3.0，则填0')
+                    return
+                }
+                if (self.eye.rightNaked != 0 && !(self.eye.rightNaked >= 3.0 && self.eye.rightNaked <= 5.3)) {
+                    alert('右眼视力录入范围3.0-5.3之间，若低于3.0，则填0')
+                    return
+                }
+                if(self.eye.leftNaked>=5.0&&self.eye.leftNaked<=5.3){
+                    self.eye.leftGlass = '0 正常'
+                    self.eye.leftAmetropia = '0 正常'
+                }
+                if(self.eye.rightNaked>=5.0&&self.eye.rightNaked<=5.3){
+                    self.eye.rightGlass = '0 正常'
+                    self.eye.rightAmetropia = '0 正常'
+                }
+                if(self.eye.leftGlass===null){
+                    alert('左眼串镜未填')
+                    return
+                }
+                if(self.eye.rightGlass===null){
+                    alert('右眼串镜未填')
+                    return
+                }
+                if(self.eye.leftAmetropia===null){
+                    alert('左眼屈光不正未填')
+                    return
+                }
+                if(self.eye.rightAmetropia===null){
+                    alert('右眼屈光不正未填')
+                    return
+                }
                 self.eye.userId = sessionStorage.getItem('jobNumber')
                 API.insertEye(self.eye)
                     .then(res => {
@@ -93,7 +145,7 @@
             this.getUserEye()
         },
         components: {
-            CellBox, Cell, Group, XHeader, XInput, FlexboxItem, Flexbox, XButton
+            CellBox, Cell, Group, XHeader, XInput, FlexboxItem, Flexbox, XButton, Selector
         }
     }
 </script>
